27ff01ba2ee8bf0dd49ca20ad5c06bf4df7e8005
robertsbaer <robertsbaer@yahoo.com>
2020-01-31 12:27:46 -0500

    small update

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hey Fucko!</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>This is page 3</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
